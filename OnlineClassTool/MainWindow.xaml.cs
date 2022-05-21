5a4bed80cd6344bc091e1a73e0b59bf13df82e36
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2022-05-21 15:38:57 +0800

    Add files via upload

---
+++ OnlineClassTool/MainWindow.xaml.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp;
+using CefSharp.Wpf;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OnlineClassTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                dynamic jsonFile = JsonConvert.DeserializeObject(File.ReadAllText("./bin/DefaultSettings.json"));
+
+                string defaultSES = (jsonFile["Settings"]["DefaultSearchEngine"]["engine"]);
+                string defaultWQS = (jsonFile["Settings"]["DefaultQuestionsWebsite"]["website"]);
+                string topMostS = (jsonFile["Settings"]["TopMost"]);
+
+                TopMostSettings.Text = topMostS;
+                if (TopMostSettings.Text == "true")
+                {
+                    c_TopMost.IsChecked = true;
+                }
+                else if (TopMostSettings.Text == "false")
+                {
+                    c_TopMost.IsChecked = false;
+                }
+                else
+                {
+                    MessageBox.Show(this, "Invalid TopMost setting\nPlease check it on /bin/DefaultSettings.json", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    this.Close();
+                }
+
+                WebsiteSettings.Text = defaultWQS;
+                if (WebsiteSettings.Text == "brainly")
+                {
+                    C_BrainlyPH.IsChecked = true;
+                }
+                else if (WebsiteSettings.Text == "quora")
+                {
+                    C_Quora.IsChecked = true;
+                }
+                else if (WebsiteSettings.Text == "scholar google")
+                {
+                    C_SCHgoogle.IsChecked = true;
+                }
+                else
+                {
+                    MessageBox.Show(this, "Invalid default questions websites\nPlease check it on /bin/DefaultSettings.json", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    this.Close();
+                }
+
+                SearchEngineSettings.Text = defaultSES;
+                if (SearchEngineSettings.Text == "google")
+                {
+                    m_browser.Load("https://google.com");
+                }
+                else if (SearchEngineSettings.Text == "bing")
+                {
+                    m_browser.Load("https://google.com");
+                }
+                else if (SearchEngineSettings.Text == "yahoo")
+                {
+                    m_browser.Load("https://google.com");
+                }
+                else if (SearchEngineSettings.Text == "yandex")
+                {
+                    m_browser.Load("https://google.com");
+                }
+                else if (SearchEngineSettings.Text == "ask")
+                {
+                    m_browser.Load("https://google.com");
+                }
+                else
+                {
+                    MessageBox.Show(this, "Invalid default search engine\nPlease check it on /bin/DefaultSettings.json", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    this.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show(this, "Something went wrong\nPlease check your internet", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Close();
+            }
+        }
+
+        private void btn_Ask_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            {
+                if (L_TextBox.Text == "")
+                {
+                    MessageBox.Show(this, "Please insert a text", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    if (C_BrainlyPH.IsChecked == true)
+                    {
+                        mitm_Using.Header = "Currently Using: Brainly";
+            
+                        ChromeDriverService webDriver1 = ChromeDriverService.CreateDefaultService();
+                        webDriver1.HideCommandPromptWindow = true;
+                        IWebDriver webDriver = new ChromeDriver(webDriver1);
+            
+                        By BrainlySearchb = By.Name("q");
+                        By BrainlySbtn = By.ClassName("search__icon--PKLvc");
+            
+                        webDriver.Manage().Window.Maximize();
+                        webDriver.Navigate().GoToUrl("https://brainly.ph/");
+            
+                        webDriver.FindElement(BrainlySearchb).SendKeys(L_TextBox.Text);
+                        webDriver.FindElement(BrainlySbtn).Click();
+                    }
+                    else if (C_Quora.IsChecked == true)
+                    {
+                        mitm_Using.Header = "Currently Using: Quora";
+            
+                        ChromeDriverService webDriver1 = ChromeDriverService.CreateDefaultService();
+                        webDriver1.HideCommandPromptWindow = true;
+                        IWebDriver webDriver = new ChromeDriver(webDriver1);
+            
+                        string URLencode = Uri.EscapeDataString("https://quora.com/search?q=" + L_TextBox.Text);
+                        string URLdecode = Uri.UnescapeDataString(URLencode);
+            
+                        webDriver.Manage().Window.Maximize();
+                        webDriver.Navigate().GoToUrl(URLdecode);
+                    }
+                    else if (C_SCHgoogle.IsChecked == true)
+                    {
+                        mitm_Using.Header = "Currently using: Scholar Google";
+
+                        ChromeDriverService webDriver1 = ChromeDriverService.CreateDefaultService();
+                        webDriver1.HideCommandPromptWindow = true;
+                        IWebDriver webDriver = new ChromeDriver(webDriver1);
+
+                        By schGoogleTB = By.ClassName("gs_in_txt gs_in_ac");
+                        By schGoogleSbtn = By.Name("btnG");
+
+                        webDriver.Manage().Window.Maximize();
+                        webDriver.Navigate().GoToUrl("https://scholar.google.com/");
+
+                        webDriver.FindElement(schGoogleTB).SendKeys(L_TextBox.Text);
+                        webDriver.FindElement(schGoogleSbtn).Click();
+                    }
+                    else
+                    {
+                        IWebDriver webDriver = new ChromeDriver();
+            
+                        MessageBox.Show(this, "Please select a website", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        webDriver.Quit();
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show(this, "Something went wrong", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void c_Google_Click(object sender, RoutedEventArgs e)
+        {
+            m_browser.Load("https://google.com/");
+            TBWebsiteChecker.Text = "google";
+        }
+
+        private void c_Bing_Click(object sender, RoutedEventArgs e)
+        {
+            m_browser.Load("https://bing.com/");
+            TBWebsiteChecker.Text = "bing";
+        }
+
+        private void c_Yahoo_Click(object sender, RoutedEventArgs e)
+        {
+            m_browser.Load("https://yahoo.com/");
+            TBWebsiteChecker.Text = "yahoo";
+        }
+
+        private void c_Yandex_Click(object sender, RoutedEventArgs e)
+        {
+            m_browser.Load("https://yandex.com/");
+            TBWebsiteChecker.Text = "yandex";
+        }
+
+        private void c_Ask_Click(object sender, RoutedEventArgs e)
+        {
+            m_browser.Load("https://ask.com/");
+            TBWebsiteChecker.Text = "ask";
+        }
+
+        private void btn_Back_Click(object sender, RoutedEventArgs e)
+        {
+            m_browser.Back();
+        }
+
+        private void btn_Front_Click(object sender, RoutedEventArgs e)
+        {
+            m_browser.Forward();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void c_TopMost_Checked(object sender, RoutedEventArgs e)
+        {
+            this.Topmost = true;
+        }
+
+        private void c_TopMost_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.Topmost = false;
+        }
+
+        private void C_BrainlyPH_Checked(object sender, RoutedEventArgs e)
+        {
+            if (C_Quora.IsChecked == true)
+            {
+                C_Quora.IsChecked = false;
+            }
+            else if (C_SCHgoogle.IsChecked == true)
+            {
+                C_SCHgoogle.IsChecked = false;
+            }
+            else
+            {
+
+            }
+
+            C_Quora.IsChecked = false;
+            mitm_Using.Header = "Currently Using: Brainly";
+        }
+
+        private void C_Quora_Checked(object sender, RoutedEventArgs e)
+        {
+            if (C_BrainlyPH.IsChecked == true)
+            {
+                C_BrainlyPH.IsChecked = false;
+            }
+            else if (C_SCHgoogle.IsChecked == true)
+            {
+                C_SCHgoogle.IsChecked = false;
+            }
+            else
+            {
+
+            }
+
+            mitm_Using.Header = "Currently Using: Quora";
+        }
+
+        private void C_SCHgoogle_Checked(object sender, RoutedEventArgs e)
+        {
+            if (C_BrainlyPH.IsChecked == true)
+            {
+                C_BrainlyPH.IsChecked = false;
+            }
+            else if (C_Quora.IsChecked == true)
+            {
+                C_Quora.IsChecked = false;
+            }
+            else
+            {
+
+            }
+
+            mitm_Using.Header = "Currently Using: Google Scholar";
+        }
+
+        private void C_BrainlyPH_Unchecked(object sender, RoutedEventArgs e)
+        {
+            mitm_Using.Header = "Currently Using: None";
+        }
+
+        private void C_Quora_Unchecked(object sender, RoutedEventArgs e)
+        {
+            mitm_Using.Header = "Currently Using: None";
+        }
+
+        private void C_SCHgoogle_Unchecked(object sender, RoutedEventArgs e)
+        {
+            mitm_Using.Header = "Currently Using: None";
+        }
+
+        private void c_TextEditor_Click(object sender, RoutedEventArgs e)
+        {
+            TextEditor textEditor = new TextEditor();
+
+            textEditor.Show();
+        }
+    }
+}
---
+++ OnlineClassTool/TextEditor.xaml.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace OnlineClassTool
+{
+    /// <summary>
+    /// Interaction logic for TextEditor.xaml
+    /// </summary>
+    public partial class TextEditor : Window
+    {
+        public TextEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.FileName = "Text File Name";
+            saveFileDialog.DefaultExt = "*.txt";
+            saveFileDialog.Filter = "Text File (.txt)|*.txt";
+
+            Nullable<bool> result = saveFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = saveFileDialog.FileName;
+                string richText = new TextRange(rTextBox.Document.ContentStart, rTextBox.Document.ContentEnd).Text;
+                File.WriteAllText(filename, richText);
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string writeTextBox = File.ReadAllText(openFileDialog.FileName);
+                rTextBox.Document.Blocks.Clear();
+                rTextBox.Document.Blocks.Add(new Paragraph(new Run(writeTextBox)));
+            }
+        }
+
+        private void MFS10_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontSize = 10;
+        }
+
+        private void MFS12_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontSize = 12;
+        }
+
+        private void MFS14_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontSize = 14;
+        }
+
+        private void MFS16_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontSize = 16;
+        }
+
+        private void MFS18_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontSize = 18;
+        }
+
+        private void MFS20_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontSize = 20;
+        }
+
+        private void MFS22_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontSize = 22;
+        }
+
+        private void MFS24_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontSize = 24;
+        }
+
+        private void MFS26_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontSize = 26;
+        }
+
+        private void MFS28_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontSize = 28;
+        }
+
+        private void MFS30_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontSize = 30;
+        }
+
+        private void FFConsolas_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontFamily = new FontFamily("Consolas");
+        }
+
+        private void FFSegeoUI_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontFamily = new FontFamily("Segeo UI");
+        }
+
+        private void FFArial_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontFamily = new FontFamily("Arial");
+        }
+
+        private void FFBahnschrift_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontFamily = new FontFamily("Bahnschrift");
+        }
+
+        private void FFYuGothicUI_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontFamily = new FontFamily("Yu Gothic UI");
+        }
+
+        private void FFVerdana_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontFamily = new FontFamily("Verdana");
+        }
+
+        private void FWUltraLight_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontWeight = FontWeights.UltraLight;
+        }
+
+        private void FWLight_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontWeight = FontWeights.Light;
+        }
+
+        private void FWNormal_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontWeight = FontWeights.Normal;
+        }
+
+        private void FWMedium_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontWeight = FontWeights.Medium;
+        }
+
+        private void FWSemiBold_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontWeight = FontWeights.SemiBold;
+        }
+
+        private void FWBold_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontWeight = FontWeights.Bold;
+        }
+
+        private void FWUltraBold_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontWeight = FontWeights.UltraBold;
+        }
+
+        private void FWBlack_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontWeight = FontWeights.Black;
+        }
+
+        private void FWHeavy_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontWeight = FontWeights.Heavy;
+        }
+
+        private void FWUltraBlack_Click(object sender, RoutedEventArgs e)
+        {
+            rTextBox.FontWeight = FontWeights.UltraBlack;
+        }
+    }
+}
